caf1e9a2e8ff28ccd9b0d598c5d87a470757f6a8
Ryan Conrad <camalot@gmail.com>
2021-12-07 15:03:00 -0600

    updae layout

--- siege-alpha-pack/settings.js
+++ siege-alpha-pack/settings.js
@@ -2,7 +2,8 @@
 window.DEFAULT_SETTINGS = {
   // fonts from https://fonts.google.com/
   FallBackFonts: [ "Roboto", "Droid Sans", "Droid Serif" ],
-  FontName: "Permanent Marker", // google web font name
+  FontName: "Source Sans Pro", // google web font name
+  FontSize: "3.5em", // font size
 
   SB_WebSocket: "ws://127.0.0.1:1377",
   SB_CustomWebSocket: "ws://127.0.0.1:4242/r6sap"
